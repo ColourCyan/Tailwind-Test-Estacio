499022606b416623370eccd7402e36b0906b6480
Hazakura19 <158540345+Hazakura19@users.noreply.github.com>
2024-03-05 13:48:03 +0800

    adding custom font family
    config.js file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Poppins: ['Poppins'],
+        RampartOne: ['Rampart One']
+      }
+    },
   },
   plugins: [],
 }
