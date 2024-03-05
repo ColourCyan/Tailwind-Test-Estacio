1fbd4cb64e052a79d2075a773c53b581bb8b761e
brioruri <158865785+brioruri@users.noreply.github.com>
2024-03-05 13:54:33 +0800

    Imported Font-Family
    Added Roboto
    Added Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto, sans-serif'"
+      }
+    },
   },
   plugins: [],
 }
