6144c53128a6344d3d62d0c8eed78ea54eebd853
brioruri <158865785+brioruri@users.noreply.github.com>
2024-03-06 16:31:36 +0800

    Added Image Folder
    Added images necessary for the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,25 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {
-      fontFamily:{
-        poppins: "'Poppins', sans-serif",
-        roboto: "'Roboto, sans-serif'"
-      }
+      fontFamily: {
+        poppins: ["'Poppins'", 'sans-serif'],
+        roboto: ["'Roboto'", 'sans-serif'],
+      },
+      colors: {
+        'primary': '#E07406',
+      },
     },
   },
   plugins: [],
 }
-
