cc53a91e9ff3d57b5ca9239de74edb710cbf4a60
Nikka Christine Vibora <Christinemijares01@gmail.com>
2024-03-12 14:12:13 +0800

    changes
    .....

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  darkMode:"class",
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        "big":"48rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+    }
   },
   plugins: [],
 }
-
