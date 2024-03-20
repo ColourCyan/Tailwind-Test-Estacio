5ede783e011b5cda60e6ce88df9346dc026a923c
cheynnn <christianemaybcomia@iskolarngbayan.pup.edu.ph>
2024-03-20 22:34:21 +0800

    presentation - grid 1 done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'bg-color': '#E8ECEC',
+        'bg-header': '#0B4A51',
+      },
+      colors: {
+        'btn-color': '#8B5032',
+        'hover-color': '#AC8071',
+        'dark-teal': '#0B4A51',
+        'potter-clay': '#8B5032',
+        'rose-white': '#FFF6F1',
+      },
+      opacity: {
+        '80': '0.8',
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'halant': ['Halant', 'serif'],
+        'hammersmith-one': ['Hammersmith One', 'sans-serif'],
+      },
+      screens: {
+        'sm': {'max': '640px'},
+        'md': {'max': '768px'},
+        'lg': {'max': '1024px'},
+        'xl': {'max': '1280px'},
+        '2xl': {'max': '1536px'},
+      },
+    },
   },
   plugins: [],
 }
